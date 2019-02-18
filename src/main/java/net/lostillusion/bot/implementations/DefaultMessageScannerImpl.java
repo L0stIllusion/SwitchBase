579c7c15f56526d7053248536dad37d68dafb04a
Lost Illusion <zctrap2@protonmail.ch>
2019-02-18 15:16:02 -0500

    Changed message scanner from using default impl of command core to user picked

--- src/main/java/net/lostillusion/bot/implementations/DefaultMessageScannerImpl.java
+++ src/main/java/net/lostillusion/bot/implementations/DefaultMessageScannerImpl.java
@@ -1,8 +1,8 @@
 package net.lostillusion.bot.implementations;
 
 import net.lostillusion.bot.BotBase;
-import net.lostillusion.bot.interfaces.MessageScanner;
 import net.lostillusion.bot.abstracts.UserMess;
+import net.lostillusion.bot.interfaces.MessageScanner;
 import org.javacord.api.event.message.MessageCreateEvent;
 
 public class DefaultMessageScannerImpl implements MessageScanner {
@@ -11,7 +11,7 @@ public class DefaultMessageScannerImpl implements MessageScanner {
     if(!event.isServerMessage() || !event.getMessageAuthor().isUser()) return;
     UserMess mess = BotBase.getUserMess(event).orElseThrow(AssertionError::new); //Something is seriously wrong if an exception is thrown
     if(mess.getPrefix() != null)  {
-      new DefaultCommandCoreImpl().getCommmands().stream()
+      BotBase.getCommandCore().getCommmands().stream()
               .filter(command -> mess.getMessageContentWithoutPrefix().split(" ")[0].equalsIgnoreCase(command.getKey()))
               .findFirst()
               .ifPresent(command -> command.execute(mess, BotBase.getApi()));
